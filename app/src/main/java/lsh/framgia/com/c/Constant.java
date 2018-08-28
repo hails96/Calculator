bfd467a54076b73d6aadfe2cc57725679f9db716
Hai Luong <luongsonhai96@gmail.com>
2018-08-28 11:24:16 +0700

    Calculator UI & compute function

---
+++ app/src/main/java/lsh/framgia/com/c/Constant.java
@@ -0,0 +1,22 @@
+package lsh.framgia.com.c;
+
+public class Constant {
+    public static final String EMPTY_STRING = "";
+    public static final String DOT = ".";
+    public static final String PERCENT = "%";
+    public static final String ZERO = "0";
+    public static final String ONE = "1";
+    public static final String TWO = "2";
+    public static final String THREE = "3";
+    public static final String FOUR = "4";
+    public static final String FIVE = "5";
+    public static final String SIX = "6";
+    public static final String SEVEN = "7";
+    public static final String EIGHT = "8";
+    public static final String NINE = "9";
+    public static final String ADD = "+";
+    public static final String SUB = "-";
+    public static final String MUL = "*";
+    public static final String DIV = "/";
+    public static final String PREF_RESULT_KEY = "result";
+}
